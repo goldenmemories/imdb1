cee3ff75413622b9d00d16ce5c4bbd4bff0b4610
goldenmemories <sunmiaodi@gmail.com>
2025-03-20 10:50:44 -0600

    Create .eslintrc.js
    Override errors with warnings

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    'no-unused-vars': 'warn', // Change from 'error' to 'warn' - won't block build
+    'react/jsx-no-target-blank': 'warn'
+  }
+};
